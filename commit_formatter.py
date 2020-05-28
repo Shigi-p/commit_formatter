9151a4759000a943f3650910f78ab9f764b43824
Shigi-p <shigi.522@gmail.com>
2020-05-28 19:50:24 +0900

    [fix] なんかコピーミスってたので修正

--- commit_formatter.py
+++ commit_formatter.py
@@ -1,5 +1,58 @@
-#!/usr/bin/env sh
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
 
-chmod +x commit_formatter.py
-echo "alias gcommit=\"python3 `pwd`/commit_formatter.py\"" >> ~/.bashrc
-source ~/.bashrc
+import subprocess
+from retry import retry
+
+
+class Commit:
+    def __init__(self):
+        self.commit_type = self.decide_commit_type()
+        self.summary = self.input_summary()
+        self.detail = self.input_detail()
+
+    @retry(ValueError)
+    def decide_commit_type(self) -> str:
+        print("0 [add]    : ファイルの追加")
+        print("1 [update] : コード・機能の追加")
+        print("2 [fix]    : コード・機能の修正")
+        print("3 [delete] : ファイルの削除")
+        print("変更の種類を選択してください : ", end="")
+        commit_type = int(input())
+        if commit_type == 0:
+            return "[add]"
+        elif commit_type == 1:
+            return "[update]"
+        elif commit_type == 2:
+            return "[fix]"
+        elif commit_type == 3:
+            return "[delete]"
+        else:
+            print("ERROR! 値が不正です")
+            raise ValueError
+
+    @retry(ValueError)
+    def input_summary(self) -> str:
+        print("サマリーを入力してください : ", end="")
+        summary = input()
+        if not summary:
+            raise ValueError
+        return summary
+
+    @retry()
+    def input_detail(self) -> str:
+        print("詳細・理由を入力してください(入力しない場合はなし) : ", end="")
+        detail = input()
+        return detail
+
+    def create_commit_message(self) -> str:
+        commit_message = self.commit_type + " " + self.summary
+        if self.detail:
+            commit_message += "\n" + "\n" + self.detail
+        print(commit_message)
+        return commit_message
+
+
+if __name__ == "__main__":
+    commit = Commit().create_commit_message()
+    subprocess.run(['git', 'commit', '-m', commit])
