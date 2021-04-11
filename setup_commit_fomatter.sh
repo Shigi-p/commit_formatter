2d8e0ee69ba92ea30d816113c104f2f49120305d
toshiki_shigihara <toshiki_shigihara@PC20054.local>
2021-04-11 11:33:14 +0900

    [refactor] bashrcからzshrcに変更
    冷静に考えたら使うの自分だけなので

--- setup_commit_fomatter.sh
+++ setup_commit_fomatter.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 chmod +x commit_formatter.py
-echo "alias gcommit=\"python3 `pwd`/commit_formatter.py\"" >> ~/.bashrc
-source ~/.bashrc
+echo "alias gcommit=\"python3 `pwd`/commit_formatter.py\"" >> ~/.zshrc
+source ~/.zshrc
